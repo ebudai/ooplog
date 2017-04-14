752a561b4c9013f64696727a188c3e77366bf26a
Eric Budai <eric_budai@hotmail.com>
2017-04-14 14:15:58 -0400

    works (except after-parsing still needs implementation) avg latency is #1, while worst case is not good (300ms)

--- ooplog.cpp
+++ ooplog.cpp
@@ -1,6 +1,11 @@
 //copyright Eric Budai 2017
+#define NOMINMAX
+
 #include <windows.h>
+#include <algorithm>
 #include "ooplog.h"
+
+#include <iostream>
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -10,9 +15,35 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
+	using namespace std;
+	using namespace std::chrono;
+
+	const char x[5] = "test";
+	const auto y = std::is_array_v<decltype(x)>;
 	spry::log log;
-	log.info("test");
-    return 0;
+	nanoseconds max_latency = 0ns;
+	nanoseconds avg_latency = 0ns;
+	log.level = spry::log::level::info;
+	//remove init penalty
+	log.info("");
+	auto iterations = 100000000;
+	for (int i = 0; i < iterations; i++)
+	{
+		//auto start = high_resolution_clock::now();
+		log.info("test");
+		/*auto end = high_resolution_clock::now();
+		auto latency = end - start;
+		avg_latency += latency;
+		max_latency = std::max(max_latency, latency);*/
+	}
+
+	/*cout << avg_latency.count() / (double)iterations << '\n';
+
+	cout << max_latency.count() << '\n';*/
+
+	//abort();
+
+	return 0;
 }
 
 
--- ooplog.h
+++ ooplog.h
@@ -1,40 +1,53 @@
 //copyright Eric Budai 2017
 #pragma once
 
-//#include "interprocess.h"
+#define NOMINMAX
+
 #include <variant>
 #include <shared_mutex>
 #include <string>
 #include <chrono>
 #include <functional>
-#include <any>
+#include <new>
 #include <Windows.h>
 
 namespace spry
 {
+	class newline { };
+
+	using arg = std::variant<newline, std::chrono::steady_clock::time_point, const char*, uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t, uint64_t, int64_t, float, double>;
+
 	struct page
 	{
-		enum { page_granularity = 1 << 16 };
+		friend struct log;
 
-		page(const char* filename, HANDLE file_handle) 
+		enum { page_granularity = 1 << 16 };
+		
+		page(const char* filename)
 			: base(nullptr)
+			, file_handle(INVALID_HANDLE_VALUE)
 			, mapping_object(INVALID_HANDLE_VALUE)
 		{
-			mapping_object = CreateFileMappingA(file_handle, nullptr, PAGE_READWRITE, 0, page_granularity, nullptr);
+			file_handle = CreateFileA(filename, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+			if (file_handle == INVALID_HANDLE_VALUE) throw std::exception("CreateFile", GetLastError());
+
+			mapping_object = CreateFileMappingA(file_handle, nullptr, PAGE_READWRITE, 0, std::numeric_limits<int32_t>::max(), nullptr);
 			if (mapping_object == INVALID_HANDLE_VALUE) throw std::exception("CreateFileMapping", GetLastError());
-			flip_to_next_page(filename);
+			
+			flip_to_next_page();			
 		}
 
 		~page()
 		{
 			UnmapViewOfFile(base);
 			CloseHandle(mapping_object);
+			CloseHandle(file_handle);
 		}
 
-		void flip_to_next_page(const char* filename)
+		void flip_to_next_page()
 		{
 			UnmapViewOfFile(base);
-			change_page(filename);
+			change_page();
 		}
 
 		uint64_t free_space() const { return page_granularity - offset; }
@@ -42,35 +55,37 @@ namespace spry
 		uint8_t* write(const uint8_t* buffer, size_t buffer_size_in_bytes)
 		{
 			auto start = base + offset;
-			std::copy(buffer, buffer + buffer_size_in_bytes, start);
 			offset += buffer_size_in_bytes;
+			std::memcpy(start, buffer, buffer_size_in_bytes);			
 			return start;
 		}
 
-		template <typename... Args> void write(Args&&... args)
+		void write(std::initializer_list<arg> args)
 		{
-			auto start = base + offset;
-			new (start) log::arg[sizeof...(args)]{ std::forward<Args>(args)... };
-			offset += sizeof...(args) * sizeof(log::arg);
+			auto start = reinterpret_cast<const uint8_t*>(args.begin());
+			write(start, args.size() * sizeof(arg));
 		}
 
 	private:
 
-		void change_page(const char* filename)
+		void change_page()
 		{
 			static uint64_t page_id = 0;
 
-			auto wide_file_offset = page_id * page_granularity;
+			auto wide_file_offset = page_id++ * page_granularity;
 			auto file_offset_high = static_cast<uint32_t>(wide_file_offset >> 32);
 			auto file_offset_low = static_cast<uint32_t>(wide_file_offset);
 
 			auto mapped_memory = MapViewOfFile(mapping_object, FILE_MAP_ALL_ACCESS, file_offset_high, file_offset_low, page_granularity);
+			if (!mapped_memory) throw std::exception("MapViewOfFile", GetLastError());
 			base = reinterpret_cast<decltype(base)>(mapped_memory);
+			offset = 0;
 		}
 
 		uint8_t* base;
 		uint64_t offset;
-		HANDLE mapping_object;		
+		HANDLE mapping_object;
+		HANDLE file_handle;
 	};
 
 	struct log
@@ -81,78 +96,63 @@ namespace spry
 
 		enum class level : int { none = 0, fatal, info, warn, debug, trace };
 
-		log(const char* filename = "spry.binlog")
-			: filename(filename)
-			, file(INVALID_HANDLE_VALUE) 
-		{
-			file = CreateFileA(filename, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
-			if (file == INVALID_HANDLE_VALUE) throw std::exception("CreateFile", GetLastError());
-		}
+		log(const char* filename = "spry.binlog") : filename(filename) { }
+		~log() = default;
 
-		~log() { CloseHandle(file); }
-
-		template <typename... Args> __forceinline void fatal(Args&&... args) 
+		template <typename... Args> inline void fatal(Args&&... args)
 		{
-			if (level == level::none) return;
-			write(clock::now(), std::forward<Args>(args)..., newline{}); 
+			if (level == level::none) return; 
+			write({ clock::now(), write_strings(std::forward<Args>(args))..., newline{} }); 
 		}
-		template <typename... Args> __forceinline void info(Args&&... args) 
+		template <typename... Args> inline void info(Args&&... args)
 		{
-			if (level < level::info) return;
-			write(clock::now(), std::forward<Args>(args)..., newline{});
+			if (level < level::info) return; 
+			write({ clock::now(), write_strings(std::forward<Args>(args))..., newline{} });
 		}
-		template <typename... Args> __forceinline void warn(Args&&... args) 
-		{
-			if (level < level::warn) return;
-			write({ clock::now(), std::forward<Args>(args)..., newline{} }); 
+		template <typename... Args> inline void warn(Args&&... args)
+		{ 
+			if (level < level::warn) return; 
+			write({ clock::now(), write_strings(std::forward<Args>(args))..., newline{} });
 		}
-		template <typename... Args> __forceinline void debug(Args&&... args) 
-		{
-			if (level < level::debug) return;
-			write({ clock::now(), std::forward<Args>(args)..., newline{} }); 
+		template <typename... Args> inline void debug(Args&&... args)
+		{ 
+			if (level < level::debug) return; 
+			write({ clock::now(), write_strings(std::forward<Args>(args))..., newline{} });
 		}
-		template <typename... Args> __forceinline void trace(Args&&... args) 
+		template <typename... Args> inline void trace(Args&&... args)
 		{
-			if (level < level::trace) return;
-			write_trace_message({ clock::now(), std::forward<Args>(args)..., newline{} }); 
+			if (level < level::trace) return; 
+			write({ clock::now(), write_strings(std::forward<Args>(args))..., newline{} });
 		}
 
 		level level;
 
 	private:
 
-		class newline { };
-
-		using arg = std::variant<newline, std::chrono::steady_clock::time_point, const char*, uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t, uint64_t, int64_t, float, double>;
-
-		template <typename... Args> __forceinline void write(Args&&... args)
+		inline void write(std::initializer_list<arg>&& args)
 		{
-			static thread_local page messages{ filename.data(), file };
+			static thread_local page messages{ filename.data() };
 			
-			write_strings(std::forward<Args>(args)...);
-			
-			messages.write(std::forward<Args>(args)...);
+			auto length = args.size() * sizeof(arg);
+			if (messages.free_space() < length) {  messages.flip_to_next_page(); }
+			messages.write(std::move(args));
 		}
 
-		template <typename T, typename... Args> __forceinline void write_strings(T&&, Args&&...) { }
-		template <typename... Args> __forceinline void write_strings(const char*& string, Args&&... args)
+		template <typename T> inline T&& write_strings(T&& arg)
 		{
-			static thread_local page strings{ (filename + "strings").data(), file };
-			static int base_pointer_saver = [&strings]()
-			{
-				strings.write(&strings.base, sizeof(strings.base)); 
-				return 0; 
-			}();
-
+			return std::forward<T>(arg);
+		}
+		
+		inline const char* write_strings(const char* string)
+		{
+			static thread_local page strings{ (filename + "strings").data() };
+			
 			auto length = std::strlen(string);
-			if (strings.free_space() < length) strings.flip_to_next_page(filename.data());
-			auto pointer = strings.write(reinterpret_cast<const uint8_t*>(string), std::strlen(string));
-			string = reinterpret_cast<const char*>(pointer);
-
-			write_strings(std::forward<Args>(args)...);
+			if (strings.free_space() < length) strings.flip_to_next_page();
+			auto pointer = strings.write(reinterpret_cast<const uint8_t*>(string), length);
+			return reinterpret_cast<const char*>(pointer);
 		}
 		
 		std::string filename;
-		HANDLE file;
 	};
 }
